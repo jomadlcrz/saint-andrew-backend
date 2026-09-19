0dfbb2b1b5c0263ff21a2a1b4c0b2d9b612f67d3
Joma Dela Cruz <jomadlcrz@gmail.com>
2026-09-20 01:38:19 +0800

    Fix duplicate health route definition in health.routes.js

--- src/routes/health.routes.js
+++ src/routes/health.routes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const healthController = require('../controllers/health.controller');
 
 router.get('/', healthController.getHealth);
+router.get('/health', healthController.getHealth);
 router.get('/test-brevo', healthController.testBrevo);
 
 module.exports = router;
